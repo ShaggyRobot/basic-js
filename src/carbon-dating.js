7c15423b810bb4b415273630a60810b6cb18d7b4
ShaggyRobot <shaggyrobot@gmail.com>
2021-03-01 19:29:42 +0300

    edit: carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -5,13 +5,10 @@ const HALF_LIFE_PERIOD = 5730
 const k = 0.693 / HALF_LIFE_PERIOD
 
 module.exports = function dateSample(sampleActivity) {
-  if (typeof(sampleActivity) != "string" && Number(sampleActivity) != 'number') {
-    console.log(">>>>>>>", sampleActivity, 'type is ',typeof(sampleActivity));
-    // console.log(">>>>>>>", typeof(sampleActivity));
+  if (typeof sampleActivity != 'string' && Number(sampleActivity) != 'number') {
     return false
   }
   let sa = Number(sampleActivity)
   let age = Math.ceil(Math.log(MODERN_ACTIVITY / sa) / k)
-  console.log(isFinite(sa), sa);
   return isFinite(age) && age > 0 ? age : false
 }
